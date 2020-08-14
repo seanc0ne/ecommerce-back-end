ecced505344e583fb29cbfbaea044e1e3ac7f3ce
Sean Cone <seandoesitbetter@gmail.com>
2020-08-14 10:43:06 -0700

    Updating tag-routes.js

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -3,14 +3,29 @@ const { Tag, Product, ProductTag } = require('../../models');
 
 // The `/api/tags` endpoint
 
-router.get('/', (req, res) => {
   // find all tags
   // be sure to include its associated Product data
+router.get('/', (req, res) => {
+  Tag.findAll({}).then(dbPost => {
+    res.json(dbPost);
+  });
 });
 
-router.get('/:id', (req, res) => {
   // find a single tag by its `id`
   // be sure to include its associated Product data
+router.get('/:id', (req, res) => {
+  Tag.findAll({
+    where: {
+      category: req.params.id
+    }
+  }).then(dbPostData => {
+    res.json(dbPostData);
+  }).catch(err => {
+    console.log(err);
+    res.status(500).json({
+      message: "This is a message of my choosing."
+    });
+  });
 });
 
 router.post('/', (req, res) => {
