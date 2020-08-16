c4a8db9d2d3fea114a1724b54eec53b2284633a9
Sean Cone <seandoesitbetter@gmail.com>
2020-08-15 19:15:19 -0700

    Update category-routes.js

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -27,7 +27,7 @@ router.get('/', (req, res) => {
 
   // find one category by its `id` value
   // be sure to include its associated Products
-router.get('/:id', (req, res, ) => {
+router.get('/:id', (req, res) => {
   CategoryfindOne({
     attributes: [
       'id',
@@ -57,26 +57,25 @@ router.get('/:id', (req, res, ) => {
 });
 
 // create a new category
-router.post('/add', async (req, res, next) => {
-  try {
-    const { name } = req.body;
-    const category = new Categories({
-      name
-    });
-    await category.save();
-    res.status(200).json(` Success `);
-  } catch (err) {
-    err.statusCode = 422;
-    next(err);
-  }
+router.post('/', (req, res) => {
+  Category.create({
+    category_name: req.body.category_name
+  })
+  .then(dbCategoryData => res.json(dbCategoryData))
+  .catch(err => {
+    console.log(err);
+    res.status(500).json(err)
+  });
 });
 
+// update a category by its `id` value
 router.put('/:id', (req, res) => {
-  // update a category by its `id` value
+
 });
 
+// delete a category by its `id` value
 router.delete('/:id', (req, res) => {
-  // delete a category by its `id` value
+
 });
 
 module.exports = router;
