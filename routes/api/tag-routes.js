a136c824fc8d69a2470e40758aa17752e80365d6
Sean Cone <seandoesitbetter@gmail.com>
2020-08-15 18:55:47 -0700

    Completing tag-routes.js

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -100,7 +100,22 @@ router.put('/:id', (req, res) => {
 
 // delete on tag by its `id` value
 router.delete('/:id', (req, res) => {
-
+  Tag.destroy({
+    where: {
+      id: req.params.id
+    }
+  })
+    .then(dbTagData => {
+      if (!dbTagData) {
+        res.status(404).json({ message: 'No tag found with this ID.' });
+        return;
+      }
+      res.json(dbTagData);
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json(err);
+    });
 });
 
 module.exports = router;
