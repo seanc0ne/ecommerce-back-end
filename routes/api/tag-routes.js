4ba472e0588576a584afe4f16eadd7de7adee989
Sean Cone <seandoesitbetter@gmail.com>
2020-08-15 18:47:58 -0700

    Update tag-routes.js

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -3,8 +3,8 @@ const { Tag, Product, ProductTag } = require('../../models');
 
 // The `/api/tags` endpoint
 
-  // find all tags
-  // be sure to include its associated Product data
+// find all tags
+// be sure to include its associated Product data
 router.get('/', (req, res) => {
   Tag.findAll({
     attributes: [
@@ -27,8 +27,8 @@ router.get('/', (req, res) => {
   });
 });
 
-  // find a single tag by its `id`
-  // be sure to include its associated Product data
+// find a single tag by its `id`
+// be sure to include its associated Product data
 router.get('/:id', (req, res) => {
   Tag.findOne({
     attributes: [
@@ -42,30 +42,44 @@ router.get('/:id', (req, res) => {
       {
         model: Product,
         attributes: ['id', 'product_name', 'price', 'stock', 'category_id'],
-        
+        through: ProductTag,
+        as: "products"
       }
     ]
   })
-  Tag.findAll({
-    where: {
-      category: req.params.id
+  .then(dbUserData => {
+    if (!dbUserData) {
+      res.status(404).json({ message: 'No tag found with this ID' });
+      return;
     }
-  }).then(dbPostData => {
-    res.json(dbPostData);
-  }).catch(err => {
-    console.log(err);
-    res.status(500).json({
-      message: "This is a message of my choosing."
+    res.json(dbUserData);
+  })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json(err);
     });
-  });
 });
 
-router.post('/', (req, res) => {
-  // create a new tag
+// create a new tag
+router.post('/', (req, res) => { 
+  Tag.create({
+    id: req.body.id,
+    tag_name: req.body.tag_name
+  })
+    .then(dbTagData => res.json(dbTagData))
+    .catch(err => {
+      console.log(err);
+      res.status(400).json(err);
+    });
 });
 
+// update a tag's name by its `id` value
 router.put('/:id', (req, res) => {
-  // update a tag's name by its `id` value
+  Tag.update(
+    {
+      tag_name: 
+    }
+  )
 });
 
 router.delete('/:id', (req, res) => {
